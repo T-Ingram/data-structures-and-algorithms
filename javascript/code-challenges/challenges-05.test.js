dda25874845e1da85767057c05dc80995f7e9232
Trevor Ingram <ingramtrevor10@gmail.com>
2024-02-11 18:07:08 -0800

    Completed challenge 2.

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -34,7 +34,8 @@ const courseInfo = { name: 'Code 301', duration: { dayTrack: '4 weeks', eveningT
 };
 
 const getCourseKeys = (obj) => {
-  // Solution code here...
+  
+  return Object.keys(obj);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -228,7 +229,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return the keys from an object', () => {
     expect(getCourseKeys(courseInfo)).toStrictEqual(['name', 'duration', 'topics', 'finalExam']);
   });
