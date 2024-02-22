0a0e123cc35b0829682e9135c0ef1e69ffa38f58
Trevor Ingram <ingramtrevor10@gmail.com>
2024-02-21 21:58:27 -0800

    Completed challenge 05

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -111,7 +111,10 @@ let starWarsData = [{
 }];
 
 const returnNames = (arr) => {
-  // Solution code here...
+  return arr.reduce((names, character) => {
+    names.push(character.name);
+    return names;
+  }, []);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -309,7 +312,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should return an array continaing the names of the characters', () => {
     expect(returnNames(starWarsData)).toStrictEqual([ 'Luke Skywalker', 'C-3PO', 'R2-D2', 'Darth Vader', 'Leia Organa' ]);
     expect(returnNames(starWarsData).length).toStrictEqual(5);
