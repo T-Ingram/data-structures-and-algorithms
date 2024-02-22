c3b03dfb9a66020b1e123a5c9982479f28fa8af4
Trevor Ingram <ingramtrevor10@gmail.com>
2024-02-21 21:44:27 -0800

    Completed challenge 03

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -39,7 +39,7 @@ Write a function named addPurchases that, given an array of objects as input, us
 ------------------------------------------------------------------------------------------------ */
 
 const addPurchases = (arr) => {
-  // Solution code here...
+  return arr.reduce((total, purchase) => total + purchase.purchasePrice, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -296,7 +296,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should add the purchase price', () => {
     expect(addPurchases([{item: 'switch', purchasePrice: 399}, {item: 'toothpaste', purchasePrice: 2}])).toStrictEqual(401);
     expect(addPurchases([])).toStrictEqual(0);
